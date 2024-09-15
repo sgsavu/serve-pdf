166eb622178686f5d59625b0208d26868e505487
Jibreel <xdarkmemoriez@gmail.com>
2024-09-15 23:07:24 +0100

    add pdf server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module serve-pdf
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var file string
+
+func main() {
+	port := flag.String("port", "8080", "Port to start the server on")
+	flag.Parse()
+	file = flag.Arg(0)
+
+	if file == "" {
+		log.Fatal("file needs to be provided")
+	}
+
+	http.HandleFunc("/", servePDF)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(fmt.Sprintf(":%s", *port), nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
+
+func servePDF(w http.ResponseWriter, r *http.Request) {
+	pdfFile, err := os.Open(file)
+	if err != nil {
+		http.Error(w, "File not found.", 404)
+		fmt.Println(err)
+		return
+	}
+	defer pdfFile.Close()
+
+	w.Header().Set("Content-Type", "application/pdf")
+
+	http.ServeFile(w, r, file)
+}
